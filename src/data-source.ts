0a440837768285f13486cf29fbddcccf5d97640b
juliocesarjcrs <jcrico27@misena.edu.co>
2024-05-22 16:12:36 -0500

    fix command run start

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,11 +1,14 @@
 import { DataSource } from 'typeorm';
 import * as path from 'path';
+
+const isCompiled = path.extname(__filename).includes('js');
+
 export const AppDataSource = new DataSource({
   type: 'sqlite',
   database: 'database.sqlite',
   synchronize: true,
   logging: true,
-  entities: [path.join(__dirname, './entity/*.ts')],
-  migrations: [path.join(__dirname, './migration/*.ts')],
+  entities: [path.join(__dirname, isCompiled ? './entity/*.js' : './entity/*.ts')],
+  migrations: [path.join(__dirname, isCompiled ? './migration/*.js' : './migration/*.ts')],
   subscribers: [],
 });
